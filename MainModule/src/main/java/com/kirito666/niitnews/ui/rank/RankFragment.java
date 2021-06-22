d070cfeced6dfdb6a17ffdef3a39e2b34557462c
Shinonon <1289037748@qq.com>
2021-06-22 14:03:45 +0800

    初步尝试MVVM架构思想写热榜模块，视图View+视图数据持有者ViewModel，由Retrofit的HTTP数据充当Model数据源Repository，尝试使用LiveData来观察Retrofit请求到的数据源

--- MainModule/src/main/java/com/kirito666/niitnews/ui/rank/RankFragment.java
+++ MainModule/src/main/java/com/kirito666/niitnews/ui/rank/RankFragment.java
@@ -1,20 +1,97 @@
 package com.kirito666.niitnews.ui.rank;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.databinding.library.baseAdapters.BR;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.kirito666.niitnews.R;
 import com.kirito666.niitnews.databinding.FragmentRankBinding;
-import com.kirshi.framework.viewbinding.BaseFragment;
+import com.kirito666.niitnews.entity.Rank;
+import com.kirito666.niitnews.ui.rank.adapter.RankListAdapter;
+import com.kirshi.framework.databinding.DataBindingConfig;
+import com.kirshi.framework.databinding.DataBindingFragment;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 /**
  * Copyright (c) 2021
  * @Project:NiitNews
- * @Author:Finger
+ * @Author:Shinonon
  * @FileName:RankFragment.java
- * @LastModified:2021/06/22 08:23:22
+ * @LastModified:2021/06/22 13:47:22
  */
 
-public class RankFragment extends BaseFragment<FragmentRankBinding> {
+public class RankFragment extends DataBindingFragment<FragmentRankBinding> {
+    private RankPageViewModel mRankPageViewModel;
+    private RankListAdapter mAdapter;
+    private final boolean isPost;
+
+    public RankFragment(boolean isPost) {
+        this.isPost = isPost;
+    }
 
     @Override
-    public void inCreateView() {
+    protected void initViewModel() {
+        mRankPageViewModel = new RankPageViewModel(isPost);
+    }
 
+    @Override
+    protected DataBindingConfig getDataBindingConfig() {
+        return new DataBindingConfig(R.layout.fragment_rank, BR.vm, mRankPageViewModel)
+                .addBindingParam(BR.click, new RankFragment.ClickProxy());
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        v.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        v.recyclerView.setHasFixedSize(true);
+        mAdapter = new RankListAdapter(getContext(), mRankPageViewModel.ranks.getValue(), R.layout.item_news_light);
+        mAdapter.setOnItemClickListener(new RankListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, Rank rank, int position) {
+                // TODO: 6/22/2021 热搜榜点击事件
+            }
+        });
+        //((SimpleItemAnimator)v.recyclerView.getItemAnimator()).setSupportsChangeAnimations(false);
+        //v.recyclerView.getItemAnimator().setChangeDuration(0);
+        v.recyclerView.setAdapter(mAdapter);
+        mRankPageViewModel.ranks.observe(getViewLifecycleOwner(), new Observer<List<Rank>>() {
+            @Override
+            public void onChanged(List<Rank> ranks) {
+                mAdapter.notifyDataSetChanged();
+                v.refreshLayout.setRefreshing(false);
+            }
+        });
+        mRankPageViewModel.fetchRank();
+        v.refreshLayout.setColorSchemeResources(R.color.google_blue,
+                R.color.google_green, R.color.google_yellow,
+                R.color.google_red);
+        v.refreshLayout.setDistanceToTriggerSync(300);
+        v.refreshLayout.setProgressBackgroundColorSchemeColor(Color.WHITE);
+        v.refreshLayout.setOnRefreshListener(() -> {
+            v.refreshLayout.setRefreshing(true);
+            if (v != null) {
+                mRankPageViewModel.fetchRank();
+            }
+        });
+    }
+
+    public static class ClickProxy implements Toolbar.OnMenuItemClickListener {
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            return true;
+        }
     }
 }
---
+++ MainModule/src/main/java/com/kirito666/niitnews/ui/rank/RankPageViewModel.java
@@ -0,0 +1,67 @@
+package com.kirito666.niitnews.ui.rank;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.kirito666.niitnews.entity.Rank;
+import com.kirito666.niitnews.entity.base.BaseResponse;
+import com.kirito666.niitnews.entity.base.HttpStatusCode;
+import com.kirito666.niitnews.net.APIService;
+import com.kirito666.niitnews.net.retrofit.RetrofitClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Copyright (c) 2021
+ * @Project:NiitNews
+ * @Author:Shinonon
+ * @FileName:RankPageViewModel.java
+ * @LastModified:2021/06/22 13:49:22
+ */
+
+public class RankPageViewModel extends ViewModel {
+    private final APIService mRepository = RetrofitClient.getInstance().getApi();
+    public MutableLiveData<List<Rank>> ranks;
+    private final boolean isPost;
+
+    public RankPageViewModel(boolean isPost) {
+        this.isPost = isPost;
+        if (ranks == null) {
+            ranks = new MutableLiveData<>();
+            ranks.setValue(new ArrayList<>());
+        } else {
+            if (ranks.getValue() != null) {
+                ranks.getValue().clear();
+            }
+        }
+    }
+
+    public void fetchRank() {
+        Call<BaseResponse<List<Rank>>> call = isPost? mRepository.fetchPostsRank(): mRepository.fetchNewsRank();
+        call.enqueue(new Callback<BaseResponse<List<Rank>>>() {
+            @Override
+            public void onResponse(Call<BaseResponse<List<Rank>>> call, Response<BaseResponse<List<Rank>>> response) {
+                if (response.body().getStatusCode() == HttpStatusCode.SUCCESS.getStatus()) {
+                    List<Rank> diffRanks = ranks.getValue();
+                    diffRanks.clear();
+                    diffRanks.addAll(response.body().getData());
+                    ranks.setValue(diffRanks);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<BaseResponse<List<Rank>>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    public void search() {
+
+    }
+}
