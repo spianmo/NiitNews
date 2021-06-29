35ec55e17641934f95589b9fbbae245ce79dbe6e
spianmo <Finger@spianmo.com>
2021-06-30 07:50:41 +0800

    完成Profile页
    Signed-off-by: spianmo <Finger@spianmo.com>

---
+++ MainModule/src/main/java/com/kirito666/niitnews/ui/profile/ProfilePage.java
@@ -0,0 +1,31 @@
+package com.kirito666.niitnews.ui.profile;
+
+import com.kirito666.niitnews.databinding.PageProfileBinding;
+import com.kirshi.framework.databinding.BaseBindingActivity;
+import com.kirshi.framework.databinding.DataBindingConfig;
+
+/**
+ * Copyright (c) 2021
+ * @Project:NiitNews
+ * @Author:Finger
+ * @FileName:ProfilePage.java
+ * @LastModified:2021/06/30 02:23:30
+ */
+
+public class ProfilePage extends BaseBindingActivity<PageProfileBinding> {
+
+    @Override
+    protected void initViewModel() {
+
+    }
+
+    @Override
+    protected DataBindingConfig getDataBindingConfig() {
+        return null;
+    }
+
+    @Override
+    protected void beforeInitViewModel() {
+
+    }
+}
